bc6fbe30e6abde12070033f5c591e02c9c232287
aarthit1015 <aarthit1015@gmail.com>
2025-10-16 20:22:11 +0530

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -10,7 +10,10 @@ DB_PORT = os.getenv("DB_PORT", "9832")
 DB_NAME = os.getenv("DB_NAME", "dev-es")
 DB_DRIVER = os.getenv("DB_DRIVER", "psycopg2")
 
-DATABASE_URL = f"postgresql+{DB_DRIVER}://{DB_USER}:{quote_plus(DB_PASS)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# DATABASE_URL = f"postgresql+{DB_DRIVER}://{DB_USER}:{quote_plus(DB_PASS)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+pg8000://{DB_USER}:{quote_plus(DB_PASS)}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
 
 try:
     engine = create_engine(DATABASE_URL, pool_pre_ping=True)
@@ -18,4 +21,5 @@ try:
 except Exception as e:
     logger.error(f" DB Connection Error: {e}")
     raise
+
 
